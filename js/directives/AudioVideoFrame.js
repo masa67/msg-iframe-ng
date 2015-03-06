21bedaadd0cf9e106d82e24bd59b4914e1d5b4ec
Matti Sangi <matti.sangi@deveteam.com>
2015-03-06 13:18:59 +0200

    url passed to audioVideoFrame directive

--- js/directives/AudioVideoFrame.js
+++ js/directives/AudioVideoFrame.js
@@ -7,7 +7,17 @@
         .module('AudioVideoFrame', [])
         .directive('audioVideoFrame', function () {
             return {
-                template: '<div>Hello World!</div>'
+                restrict: 'E',
+                scope: {
+                    url: '='
+                },
+                template:
+                    '<div id="video-block-outer">' +
+                        '<div id="video-block-inner" class="video-block-inner">' +
+                            '<iframe id="video-block-iframe" class="video-block-iframe" scrolling="no" frameborder="0"' +
+                                'src="{{url}}"></iframe>' +
+                        '</div>' +
+                    '</div>'
             };
         });
 }());
