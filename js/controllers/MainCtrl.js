a31e7a02afcbd6ca19f8cc8e0194626927659191
Matti Sangi <matti.sangi@pp.inet.fi>
2015-03-06 21:26:35 +0200

    cleanup

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -5,47 +5,14 @@
     /*global angular, Modernizr, document, $ */
     angular
         .module('MainPage', [])
-        .controller('MainController', [ '$scope', function ($scope) {
-
-            $scope.iframeUrl = 'http://localhost:8080/test4/index2.html';
-
-            function setIframeWidth() {
-                var w = document.getElementById('video-block-outer').offsetWidth;
-                document.getElementById('video-block-iframe').width = w;
-            }
-
-            function sendConfigToIframe() {
-                var w = document.getElementById('video-block-iframe').contentWindow;
-                w.postMessage($scope.iframeCfg, 'http://localhost:8080/test4/index2_nodir.html');
-            }
-
-            $scope.clickConfig = function () {
-                sendConfigToIframe();
-            };
-
-            setIframeWidth();
-            $(window).resize('resize.doResize', function () {
-                setIframeWidth();
-            });
+        .controller('MainController', [ '$scope', '$location', function ($scope, $location) {
 
             $scope.iframeCfg = {
                 blocks: 'both'
             };
 
-            $scope.$on('msg', function (event, args) {
-                if (args.source !== 'no-directive') {
-                    return;
-                }
-
-                // '8' below is due to padding added on this page (i.e., not a miscalculation in the iframe).
-                document.getElementById('video-block-inner').style.width = (args.width + 8) + 'px';
-                document.getElementById('video-block-inner').style.height = (args.height + 4 + 8) + 'px';
-                document.getElementById('video-block-iframe').height = args.height;
-            });
-
-            $scope.$on('$destroy', function () {
-                $(window).off('resize.doResize');
-            });
-
+            var l = document.createElement("a");
+            l.href = $location.absUrl();
+            $scope.iframeUrl = $location.absUrl().replace(l.hash, '') + 'index2.html';
         }]);
 }());
