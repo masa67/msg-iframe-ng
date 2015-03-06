9476dbe1005351c5789b1c84a8ec53d8e33fb430
Matti Sangi <matti.sangi@deveteam.com>
2015-03-06 12:18:50 +0200

    v2.1: improvements

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -5,11 +5,21 @@
     /*global angular, Modernizr */
     angular
         .module('MainPage', [])
-        .controller('MainController', [ '$scope', '$window', function ($scope, $window) {
+        .controller('MainController', [ '$scope', function ($scope) {
 
             function setIframeWidth() {
                 var w = document.getElementById('video-block-outer').offsetWidth;
                 document.getElementById('video-block-iframe').width = w;
+            }
+
+            function sendConfigToIframe() {
+                var w = document.getElementById('video-block-iframe').contentWindow;
+
+                w.postMessage($scope.iframeCfg, 'http://localhost:63342/msg-iframe-ng/index2.html');
+            }
+
+            $scope.clickConfig = function () {
+                sendConfigToIframe();
             }
 
             setIframeWidth();
@@ -17,10 +27,15 @@
                 setIframeWidth();
             });
 
+            $scope.iframeCfg = {
+                blocks: 'both'
+            };
+
             $scope.$on('msg', function (event, args) {
                 // '8' below is due to padding added on this page (i.e., not a miscalculation in the iframe).
                 document.getElementById('video-block-inner').style.width = (args.width + 8) + 'px';
                 document.getElementById('video-block-inner').style.height = (args.height + 4 + 8) + 'px';
+                document.getElementById('video-block-iframe').height = args.height;
             });
 
             $scope.$on('$destroy', function () {
