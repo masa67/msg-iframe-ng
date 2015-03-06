29976aa4ee2eee2d6d9eeb2218b6735d178a8504
Matti Sangi <matti.sangi@deveteam.com>
2015-03-06 14:19:03 +0200

    For incoming messages on parent, the source hostname is checked

--- js/directives/AudioVideoFrame.js
+++ js/directives/AudioVideoFrame.js
@@ -2,34 +2,31 @@
 (function () {
     'use strict';
 
-    /*global angular, window, $, addEventListener, attachEvent */
+    /*global angular, window, $, console */
     angular
         .module('AudioVideoFrame', [])
         .directive('audioVideoFrame', ['$window', function ($window) {
 
-            /*
-            function listener(event) {
-                //if (event.origin !== "http://localhost:63342/" ) {
-                //    return;
-                //}
-                angular.element($('*[ng-app]')).scope().$broadcast('msg', event.data);
+            var srcHost;
+
+            function getHost(href) {
+                var l = document.createElement("a");
+                l.href = href;
+                return l.host;
             }
 
-            if (window.addEventListener) {
-                addEventListener("message", listener, false);
-            } else {
-                attachEvent("onmessage", listener);
-            }
-            */
-
-            angular.element($window).on('message', function(e) {
-                //if (event.origin !== "http://localhost:63342/" ) {
-                //    return;
-                //}
+            angular.element($window).on('message', function () {
+                if (srcHost !== getHost(event.origin)) {
+                    console.log('Host mismatch: srcHost = ' + srcHost + ', evHost = ' + getHost(event.origin));
+                    return;
+                }
                 angular.element($('*[ng-app]')).scope().$broadcast('msg', event.data);
             });
 
             return {
+                link: function (scope, elem, attrs) {
+                    srcHost = getHost(scope.url);
+                },
                 restrict: 'E',
                 scope: {
                     url: '='
@@ -37,7 +34,8 @@
                 template:
                     '<div id="video-block-outer">' +
                         '<div id="video-block-inner" class="video-block-inner">' +
-                            '<iframe id="video-block-iframe" class="video-block-iframe" scrolling="no" frameborder="0"' +
+                            '<iframe ' +
+                                'id="video-block-iframe" class="video-block-iframe" scrolling="no" frameborder="0"' +
                                 'src="{{url}}"></iframe>' +
                         '</div>' +
                     '</div>'
